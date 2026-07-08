70d9204340e7955dea2b2faf13d9f68f75221246
Pavel Bezborodov <dfa.sro591@gmail.com>
2026-07-08 21:50:06 +0200

    Bumping version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.hazzus.karooclimber"
         minSdk = 26 // Karoo 2 (Android 8)
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
     }
 
     signingConfigs {
