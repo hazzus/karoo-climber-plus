64a7318f0119db4bb234dc4ec411c5c1aa7d63e4
Pavel Bezborodov <dfa.sro591@gmail.com>
2026-07-08 11:41:35 +0200

    New version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.hazzus.karooclimber"
         minSdk = 26 // Karoo 2 (Android 8)
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     signingConfigs {
